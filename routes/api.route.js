134b65dcf38cb3f0831c64c4a6174ca6d7d5dc23
snapper <zihanjashim2121@gmail.comm>
2022-09-25 01:38:23 +0600

    first commit

---
+++ routes/api.route.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+router.get('/products', async (req, res, next) => {
+  try {
+    const products = await prisma.product.findMany({
+      include: { category: true },
+    })
+
+    const categories = await prisma.category.findMany({
+      include: { products: true },
+    })
+
+    res.json({ products, categories })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/products:id', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' })
+})
+
+router.post('/products', async (req, res, next) => {
+  try {
+    const product = await prisma.product.create({
+      data: req.body,
+    })
+    res.json(product)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/products/:id', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' })
+})
+
+router.patch('/products/:id', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' })
+})
+
+module.exports = router
