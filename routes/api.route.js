4534374f055869f3b43115f9c6a00c1cc3fed240
snapper <zihanjashim2121@gmail.comm>
2022-09-26 00:44:05 +0600

    DELETE, FETCH and Model modifying added

--- routes/api.route.js
+++ routes/api.route.js
@@ -46,11 +46,35 @@ router.post('/products', async (req, res, next) => {
 })
 
 router.delete('/products/:id', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' })
+  try {
+    const { id } = req.params
+    const deletedProduct = await prisma.product.delete({
+      where: {
+        id: Number(id),
+      },
+    })
+    res.json(deletedProduct)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.patch('/products/:id', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' })
+  try {
+    const { id } = req.params
+    const product = await prisma.product.update({
+      where: {
+        id: Number(id),
+      },
+      data: req.body,
+      include: {
+        category: true,
+      },
+    })
+    res.json(product)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
